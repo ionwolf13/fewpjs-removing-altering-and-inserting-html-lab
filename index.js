27f07348cfd6fe53bfa77dd78750713a4dc892d0
ionwolf13 <voli.scorp13@gmail.com>
2021-01-12 17:44:08 -0600

    commit tree 26232ad03cdfcd0d570c5e12caee3619873b7562

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+// let head = document.querySelector('head')
+// head.appendChild(newHeader)
+newHeader.innerHTML = "YOUR-NAME is the champion"
